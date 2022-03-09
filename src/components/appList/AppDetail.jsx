abab442194848736adb0837c0fb3f82a03e9b77c
Juliette Mahé <mahe.juliette@outlook.com>
2022-03-09 15:30:01 +0100

    new flexbox

--- src/components/appList/AppDetail.jsx
+++ src/components/appList/AppDetail.jsx
@@ -6,25 +6,25 @@ const AppDetail = (props) => {
 
     return (
         <div className="AppDetail">
-            <div className="photoBox">
+            <div className="firstBox">
                 <div className="mainImgContainer">
                     <img className="mainImg" src={props.globalpic1} alt={props.name} />
                 </div>
-                <div className="otherImg">
-                    <img className="app-secondary-img" src={props.globalpic2} alt={props.name} />
-                    <img className="app-secondary-img" src={props.globalpic3} alt={props.name} />
-                    <img className="app-secondary-img" src={props.globalpic4} alt={props.name} />
-                    <img className="app-secondary-img" src={props.globalpic5} alt={props.name} />
-                </div>   
-            </div>  
-            <div className='text-content'>       
-            <p className='app-name'>{props.name}</p>
-            <div className='app-text'>
-            <p className='app-description'>{props.description}</p>
-            <p className='app-client'>Réalisé pour un client {props.clienttype}.</p>  
-            <p className='app-stack'>Ici la stack techno </p>
-            </div>
-            </div>
+                <div className='text-content'>       
+                    <p className='app-name'>{props.name}</p>
+                    <div className='app-text'>
+                       <p className='app-description'>{props.description}</p>
+                       <p className='app-client'>Réalisé pour un client {props.clienttype}.</p>  
+                       <p className='app-stack'>Ici la stack techno </p>
+                    </div>
+                </div> 
+            </div>              
+            <div className="secondBox">
+                <img className="app-secondary-img" src={props.globalpic2} alt={props.name} />
+                <img className="app-secondary-img" src={props.globalpic3} alt={props.name} />
+                <img className="app-secondary-img" src={props.globalpic4} alt={props.name} />
+                <img className="app-secondary-img" src={props.globalpic5} alt={props.name} />
+            </div> 
         </div>
     )
 }
